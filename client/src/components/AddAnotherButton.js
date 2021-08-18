0e461541ee034cd21eec229d7bfae42945d630f9
Jakob <me@jterhorst.de>
2021-08-18 17:13:49 +0200

    Fix Cardcomponent to improved version

--- client/src/components/AddAnotherButton.js
+++ client/src/components/AddAnotherButton.js
@@ -3,8 +3,10 @@ import { ReactComponent as AddAnotherButtonIcon } from "../svg/icon-add.svg";
 
 export default function AddAnotherButton() {
   return (
-    <div className="ForwardButton">
-      <AddAnotherButtonIcon className="lineIcon button--add icon opaque" />
-    </div>
+    <header className="Card__Header">
+      <div className="ForwardButton">
+        <AddAnotherButtonIcon className="lineIcon button--add icon opaque" />
+      </div>
+    </header>
   );
 }
