4539081d88728c9c6c05dce1201c7ae140077453
Jakob ter Horst <me@jterhorst.de>
2021-08-29 21:01:39 +0200

    Controlled Component Action

--- client/src/pages/Create.js
+++ client/src/pages/Create.js
@@ -5,20 +5,27 @@ import HeaderCreateCaseGoForward from "../components/Card_components/HeaderCreat
 import SpeechInput from "../components/SpeechInput";
 // ============================ Import Requirements
 import { useState, useEffect } from "react";
-import { Switch, Route, Link, useParams, useHistory } from "react-router-dom";
+import {
+  Switch,
+  Route,
+  Link,
+  useParams,
+  useHistory,
+  useLocation,
+} from "react-router-dom";
 import { v4 as uuidv4 } from "uuid";
 
 export default function CreateCase({ isLogin, supportsSpeech }) {
+  // ========================================== Case
   const [isTooShort, setIsTooShort] = useState(true);
-
-  const [triggerInput, setTriggerInput] = useState("");
-  const [eventTriggerInput, setEventTriggerInput] = useState("");
 
   const [caseInput, setCaseInput] = useState({
     reminderId: uuidv4(),
     trigger: "",
     eventTrigger: "",
   });
+  const [triggerInput, setTriggerInput] = useState("");
+  const [eventTriggerInput, setEventTriggerInput] = useState("");
 
   useEffect(() => {
     setCaseInput({
@@ -29,13 +36,71 @@ export default function CreateCase({ isLogin, supportsSpeech }) {
     setIsTooShort(!triggerInput || !eventTriggerInput);
   }, [triggerInput, eventTriggerInput]);
 
-  const [actionInput, setActionInput] = useState({});
+  // ============================== Action
+  const { taskId } = useParams();
+  const location = useLocation();
+  console.log(location);
+  console.log(taskId);
+  const [verbInput, setVerbInput] = useState("");
+  const [actionInput, setActionInput] = useState("");
+  const [withInput, setWithInput] = useState("");
+  const [action, setAction] = useState({
+    taskId,
+    verb: "",
+    action: "",
+    with: "",
+  });
 
+  useEffect(() => {
+    setAction({
+      ...action,
+      verb: verbInput,
+      action: actionInput,
+      with: withInput,
+    });
+
+    setIsTooShort(!verbInput || !actionInput);
+  }, [verbInput, actionInput, withInput]);
+  console.log(action);
   return (
     <Switch>
-      <Route path="/create/:taskid">
+      <Route path="/create/:taskId">
         <main id="CreateAction" className="card-screen">
-          <Card> Task</Card>
+          <Card>
+            <h2> Do this</h2>
+            <form id="createForm" className="dispFlex margin-b--l">
+              <SpeechInput
+                label="Verb"
+                value={verbInput}
+                onChange={(event) => setVerbInput(event.target.value)}
+                supportsSpeech={supportsSpeech}
+                // onMouseDown={onSpeechInput}
+                // disable={disable}
+              />
+              <SpeechInput
+                label="Action"
+                value={actionInput}
+                onChange={(event) => setActionInput(event.target.value)}
+                supportsSpeech={supportsSpeech}
+                // onMouseDown={onSpeechInput}
+                // disable={disable}
+              />
+              <SpeechInput
+                label="With"
+                value={withInput}
+                onChange={(event) => setWithInput(event.target.value)}
+                supportsSpeech={supportsSpeech}
+                required={false}
+                // onMouseDown={onSpeechInput}
+                // disable={disable}
+              />
+              {isTooShort && (
+                <p className="Card__message--error">
+                  Required Input fields can't be empty
+                </p>
+              )}
+            </form>
+          </Card>
         </main>
       </Route>
 
