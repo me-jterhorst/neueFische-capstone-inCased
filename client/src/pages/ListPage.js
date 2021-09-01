d609a38782728a9fe05b03eae71509b18010da65
Jakob <me@jterhorst.de>
2021-09-01 12:56:05 +0200

    Add Listoverview Screen, Add function to scroll through List

--- client/src/pages/ListPage.js
+++ client/src/pages/ListPage.js
@@ -1,4 +1,85 @@
-export default function ListPage(inputValue) {
-  console.log(inputValue);
-  return <div> Hello Overview</div>;
+import "./ListPage.css";
+// import components
+import { ReactComponent as ForwardButtonIcon } from "../svg/icon-chevron-right.svg";
+// import requirements
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+
+export default function ListPage({ inputValue, userReminders }) {
+  const [reminders, setReminders] = useState(userReminders);
+
+  function handleSearch(inputValue) {
+    if (inputValue) {
+      const filteredReminders = reminders.filter((reminder) => {
+        return (
+          reminder.trigger.toLowerCase().includes(inputValue.toLowerCase()) ||
+          reminder.tasks.some((task) =>
+            task.with.toLowerCase().includes(inputValue.toLowerCase())
+          )
+        );
+      });
+      setReminders(filteredReminders);
+    } else {
+      setReminders(userReminders);
+    }
+  }
+
+  function renderList() {
+    return reminders.map((reminder, index) => {
+      return (
+        <section
+          className={`Overview__card dispFlex ${
+            !reminder.trigger.toLowerCase().includes(inputValue.toLowerCase())
+              ? "withHighlight"
+              : ""
+          } `}
+          key={index}
+        >
+          <div className="Overview__card__number">{reminder.tasks.length}</div>
+          <div className="Overview__card__txt">
+            <p> In case of:</p>
+            <h4>{reminder.trigger}</h4>
+            <h5>{reminder.triggerEvent}</h5>
+            {!reminder.trigger
+              .toLowerCase()
+              .includes(inputValue.toLowerCase()) && (
+              <p>Found as optional party</p>
+            )}
+          </div>
+
+          <Link
+            to={`/overview/task/${index}/0`}
+            className="Overview__card__link"
+          >
+            <ForwardButtonIcon className="icon--dark opaque" />
+          </Link>
+        </section>
+      );
+    });
+  }
+
+  useEffect(() => {
+    handleSearch(inputValue);
+  }, [inputValue]);
+
+  /**
+   * TO DO:
+   * Handle Delete Function to remove whole list Item and update
+   * App -> user.reminders & localStorage
+   *
+   * Figure out a way to include the UseEffect dependancy
+   *
+   */
+
+  return (
+    <article
+      className={`Overview__List  ${reminders.length > 4 && "List--scroll"}`}
+    >
+      {reminders.length > 0 ? (
+        renderList()
+      ) : (
+        <h1 className="Greeting">No reminders available</h1>
+      )}
+    </article>
+  );
 }
--- client/src/pages/Overview.js
+++ client/src/pages/Overview.js
@@ -5,9 +5,10 @@ import Searchfield from "../components/Searchfield";
 import BottomNav from "../components/BottomNav";
 // ======================= import pages
 import ListPage from "./ListPage";
+import SinglePage from "./SinglePage";
 // ======================== import requirements
-import { Switch, Route, Link } from "react-router-dom";
-import { useState } from "react";
+import { Switch, Route } from "react-router-dom";
+// import { useState } from "react";
 
 export default function Overview({
   searchquery,
@@ -18,6 +19,7 @@ export default function Overview({
   return (
     <Switch>
       <Route path="/overview/task/:reminderId/:postId">
+        <SinglePage />
         <BottomNav hasSpeech={false} />
       </Route>
 
@@ -28,7 +30,7 @@ export default function Overview({
             onSubmit={onSubmit}
             onChange={onSearch}
           />
-          <ListPage inputValue={searchquery} />
+          <ListPage inputValue={searchquery} userReminders={userReminders} />
         </main>
       </Route>
     </Switch>
